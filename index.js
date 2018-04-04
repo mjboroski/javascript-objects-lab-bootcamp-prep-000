5a803ec022571643df94f78614fa19c5e81a7a4d
Michael Boroski <mjboroski@gmail.com>
2018-04-04 20:32:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
   var obj = Object.assign({}, object);
-  obj = {[key]:value};
+  obj = {key:value};
   return obj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  object = {[key]:value};
+  object = {key:value};
   return object;
 }
 
